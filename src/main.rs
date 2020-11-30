718b4a529b3bfda85b558923c7597e0264bb0976
Chris Thurber <skiman6010@gmail.com>
2020-11-29 21:24:16 -0500

    Testing exercises

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use structopt::StructOpt;
+use anyhow::{Context, Result};
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
-#[derive(Debug)]
 struct Cli {
     /// The pattern to look for
     pattern: String,
@@ -11,14 +11,19 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::from_args();
     let content = std::fs::read_to_string(&args.path)
-    .expect("could not read file");
-    
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+        .with_context(|| format!("could not read file `{}`", args.path.display()))?;
+
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
+    Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    grrs::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
